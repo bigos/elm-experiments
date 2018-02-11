b91216d31a35f45506b294cee24b4e3be0c77fa6
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-11 23:08:24 +0000

    stuck

--- counter/app/elm/Main.elm
+++ counter/app/elm/Main.elm
@@ -1,10 +1,13 @@
 module Main exposing (main)
 
-import Html exposing (Html, text, div, button)
+import Html exposing (Html, text, div, button, hr, span)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
+import Array exposing (..)
+import List exposing (..)
 
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
         { model = initalModel
@@ -19,12 +22,24 @@ main =
 
 type alias Model =
     { value : Int
+    , counterIndexes : List Int
+    , counterValues : Array Int
+    , currentCounter : Int
     }
 
+-- maximum of counters starting from 0
+maxCounter : Int
+maxCounter = 2
+
+counters : Array Int
+counters = Array.repeat (maxCounter + 1) 0
 
 initalModel : Model
 initalModel =
     { value = 0
+    , counterIndexes = List.range 0 maxCounter
+    , counterValues = counters
+    , currentCounter = 0
     }
 
 
@@ -35,6 +50,10 @@ initalModel =
 type Msg
     = Increment
     | Decrement
+    | Increment5
+    | Decrement5
+    | Inc
+    | Dec
 
 
 update : Msg -> Model -> Model
@@ -42,22 +61,49 @@ update msg model =
     case msg of
         Increment ->
             { model | value = model.value + 1 }
+        Increment5 ->
+            { model | value = model.value + 5 }
 
         Decrement ->
             { model | value = model.value - 1 }
+        Decrement5 ->
+            { model | value = model.value - 5 }
+
+        Inc ->
+            { model | counterValues = (incElem model.counterValues model.currentCounter) }
+        Dec ->
+            { model | counterValues = (decElem model.counterValues model.currentCounter) }
 
 
+-- TODO: finish those two functions
+incElem vs c =
+    get c vs |> (\v -> set c (v+1) vs)
+
+
+decElem vs c =
+    vs
 
 -- View
 
+
+viewCounter model current =
+  div[] [ button [ onClick Inc] [text "+1"]
+        , span   [] [text (toString (get current model.counterValues))]
+        , button [ onClick Dec] [text "-1"]
+        ]
 
 view : Model -> Html Msg
 view model =
     div []
         [ div [ class "counter" ]
-            [ text (toString model.value) ]
+              [ text (toString model.value) ]
         , div [ class "controls" ]
-            [ button [ onClick Increment ] [ text "+1" ]
-            , button [ onClick Decrement ] [ text "-1" ]
+            [ button [ onClick Increment5 ] [ text "+5" ]
+            , button [ onClick Increment ]  [ text "+1" ]
+            , button [ onClick Decrement ]  [ text "-1" ]
+            , button [ onClick Decrement5 ] [ text "-5" ]
             ]
+
+        , hr [] []
+        , div [] (List.map (\x -> viewCounter model x ) model.counterIndexes  )
         ]
